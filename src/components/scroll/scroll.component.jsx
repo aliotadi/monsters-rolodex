9a5c9d48a70ba27d3bc97c5cddadd39727941b33
ali otadi <ali.78.otadi@gmail.com>
2020-04-27 18:34:28 +0430

    just fix the padding top issue in scroll component :))

--- src/components/scroll/scroll.component.jsx
+++ src/components/scroll/scroll.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Scroll = ({ children }) => {
   return (
     <div
-      style={{ overflow: "scroll", border: "5px solid black", height: "800px" }}
+      style={{ overflow: "scroll", paddingTop: "20px", border: "5px solid black", height: "800px" }}
     >
       {children}
     </div>
